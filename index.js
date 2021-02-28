4954ffaa2929086e6944ac80ecca548e496ceee3
Guy Arieli <guyarieli17@gmail.com>
2021-03-01 00:15:02 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,64 +1,63 @@
-const fileUpload = document.getElementById('fileUpload');
+const imageUpload = document.getElementById('fileUpload')
 
 Promise.all([
   faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
   faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
-  faceapi.nets.ssdMobilenetv1.loadFromUri('/models')
+  faceapi.nets.ssdMobilenetv1.loadFromUri('/models'),
 ]).then(start)
 
 async function start() {
-  document.body.append('Loading');
   const container = document.createElement('div');
   container.style.position = 'relative';
   document.body.append(container);
-  const labeledFaceDescription = await loadLabeledImages();
-  const faceMatcher = new faceapi.FaceMatcher(labeledFaceDescription,0.6);
-  let convertedImage;
+  const labeledFaceDescriptors = await loadLabeledImages();
+  const faceMatcher = new faceapi.FaceMatcher(labeledFaceDescriptors, 0.6);
+  let image;
   let canvas;
-  fileUpload.addEventListener('change',async()=>{
-      if(convertedImage)convertedImage.remove();
-      if(canvas)canvas.remove();
-      convertedImage = await faceapi.bufferToImage(fileUpload.files[0]);
-      container.append(convertedImage);
-      canvas = faceapi.createCanvasFromMedia(convertedImage);
-      container.append(canvas);
-      const displaySize = { width: convertedImage.width, height: convertedImage.height};
-      faceapi.matchDimensions(canvas,displaySize);
-      const detection = await faceapi.detectAllFaces(convertedImage).withFaceLandmarks().withFaceDescriptors();
-      const resizeDetection = faceapi.resizeResults(detection,displaySize);
-      const results = resizeDetection.map(d=>faceMatcher.findBestMatch(d.descriptor));
-      results.forEach((result,index) =>{
-        const box = resizeDetection[i].detection.box;
-        const drawBox = new faceapi.draw.DrawBox(box,{label: result.toString()});
-        drawBox.draw(canvas);
-      })
-     
-  });
+  document.body.append('Loaded');
+  imageUpload.addEventListener('change', async () => {
+    if (image) image.remove();
+    if (canvas) canvas.remove();
+    image = await faceapi.bufferToImage(imageUpload.files[0]);
+    container.append(image);
+    canvas = faceapi.createCanvasFromMedia(image);
+    container.append(canvas);
+    const displaySize = { width: image.width, height: image.height };
+    faceapi.matchDimensions(canvas, displaySize);
+    const detections = await faceapi.detectAllFaces(image).withFaceLandmarks().withFaceDescriptors();
+    const resizedDetections = faceapi.resizeResults(detections, displaySize);
+    const results = resizedDetections.map(d => faceMatcher.findBestMatch(d.descriptor));
+    results.forEach((result, i) => {
+      const box = resizedDetections[i].detection.box;
+      const drawBox = new faceapi.draw.DrawBox(box, { label: result.toString() });
+      drawBox.draw(canvas);
+    })
+  })
 }
 
-function loadLabeledImages(){
-  const labels = [  'Benjamin Netanyahu',
-                    'Ehud Olmert',
-                    'Ariel Sharon',
-                    'Ehud Barak',
-                    'Shimon Peres',
-                    'Yitzhak Rabin',
-                    'Yitzhak Shamir',
-                    'Menachem Begin',
-                    'Golda Meir',
-                    'David Ben-Gurion',
-                    'Moshe Sharett',
-                  ];
-  return Promise.all([
-    labels.map(async label =>{
-      const description = [];
-      for(let i=0; i<=2;i++){
-          const image = await faceapi.fetchImages(`https://github.com/GuyArieli17/prime-minister-classification/tree/main/labeled_images/${label}/${i}.png`);
-          const detections = await faceapi.detectSingleFace(image).withFaceLandmarks().withFaceDescriptors();
-          description.push(detections.descriptor);
+function loadLabeledImages() {
+  const labels = [  
+    'Benjamin Netanyahu',
+    'Ehud Olmert',
+    'Ariel Sharon',
+    'Ehud Barak',
+    'Shimon Peres',
+    'Yitzhak Rabin',
+    'Yitzhak Shamir',
+    'Menachem Begin',
+    'Golda%20Meir',
+    'David Ben-Gurion',
+    'Moshe Sharett',
+  ];
+  return Promise.all(
+    labels.map(async label => {
+      const descriptions = []
+      for (let i = 1; i <= 2; i++) {
+        const img = await faceapi.fetchImage(`https://raw.githubusercontent.com/GuyArieli17/prime-minister-classification/main/labeled_images/${label}/${i}.png`);
+        const detections = await faceapi.detectSingleFace(img).withFaceLandmarks().withFaceDescriptor();
+        if(detections)descriptions.push(detections.descriptor);
       }
-      return new faceapi.LabeledFaceDescriptors(label,detections.descriptor)
+      return new faceapi.LabeledFaceDescriptors(label, descriptions)
     })
-  ]);
-  
+  )
 }
